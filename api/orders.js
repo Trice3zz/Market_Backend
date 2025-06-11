c029bc9575f31668135bfa63cc2fe53142517f92
Danielle <dani.camp1125@gmail.com>
2025-06-10 22:03:30 -0400

    pushing product changes

--- api/orders.js
+++ api/orders.js
@@ -1,10 +1,1 @@
-import express from "express";
-import { createUser, getUser, getUsers, deleteUsers, updateUser } 
-
-const router = express.Router();
-
-router.route("/").get(async (req, res) => {
-    const movies = await getUsers();
-    res.send(users);
-})
 
---
+++ api/products.js
@@ -0,0 +1,33 @@
+import express from "express";
+import { getProductById, getReviewsByProductId } 
+
+const router = express.Router();
+
+
+router.route("/").get(async (req, res) => {
+    const products = await getProducts();
+    res.send(products);
+});
+
+router.route("/:id").get(async (req, res) => {
+    const id = req.params.id
+    const product = await getProduct(id)
+    if(!product){
+        return res.status(404).send({ error: "ID does not exist"})
+    }
+    res.send(product);
+});
+
+
+
+router.get("/products/:id/reviews", async (req, res) => {
+    const productId = req.params.id;
+    const product = await getProductById(productId);
+    if (!product) {
+        return res.status(404).send({ error: "Product not found" });
+    }
+    const reviews = await getReviewsByProductId(productId);
+    res.send(reviews);
+});
+
+export default router;
