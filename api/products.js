b26509f7ed981e7c1ac62543801f566a988ffb17
Danielle <dani.camp1125@gmail.com>
2025-06-12 16:34:47 -0400

    Small route change

--- api/products.js
+++ api/products.js
@@ -3,7 +3,7 @@ import { getProducts, getProduct } from "../db/queries/products.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.route("/").get(async (req, res) => {
   const products = await getProducts()
   res.send(products)
 });
