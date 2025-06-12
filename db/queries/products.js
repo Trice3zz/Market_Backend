c425a39a38cb5eee80eb5391dcba1e1412bc6198
Danielle <dani.camp1125@gmail.com>
2025-06-12 17:46:14 -0400

    added query paths for products --Dani

---
+++ db/queries/products.js
@@ -0,0 +1,15 @@
+import client from "/client.js";
+
+export async function getProducts(){
+    const sql = 
+    `SELECT * FROM products;`
+    const {rows: products} = await client.query(sql)
+    return products;
+};
+
+export async function getProduct(id){
+    const sql =
+    `SELECT * FROM products WHERE id = $1;`
+    const {rows: product} = await client.query(sql, [id])
+    return product[0];
+};
