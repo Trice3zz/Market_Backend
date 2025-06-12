4cc907b389093cf472f2ab834a9583a455bedcc5
Danielle <dani.camp1125@gmail.com>
2025-06-12 18:01:44 -0400

    Making code pretty --Dani

--- db/queries/products.js
+++ db/queries/products.js
@@ -1,15 +1,17 @@
 import client from "../client.js";
 
 export async function getProducts(){
-    const sql = 
-    `SELECT * FROM products;`
+    const sql = `
+    SELECT * FROM products;
+    `
     const {rows: products} = await client.query(sql)
     return products;
 };
 
 export async function getProduct(id){
-    const sql =
-    `SELECT * FROM products WHERE id = $1;`
+    const sql = `
+    SELECT * FROM products WHERE id = $1;
+    `
     const {rows: product} = await client.query(sql, [id])
     return product[0];
 };
