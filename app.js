61cbd23253c61c24d73f3575c3386c5225b31d81
Danielle <dani.camp1125@gmail.com>
2025-06-17 21:31:10 -0400

    important installs and functioning app.js so that it is correctly listening on port 3000--Dani

--- app.js
+++ app.js
@@ -1,3 +1,23 @@
-import express from "express";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import usersRouter from './api/users.js';
+import productsRouter from './api/products.js';
+import ordersRouter from './api/orders.js';
+
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+app.use('/orders', ordersRouter);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send("Sorry! Something went wrong");
+});
+
+
 export default app;
--- server.js
+++ server.js
@@ -1,17 +1,10 @@
-import app from "./app.js";
-import client from "./db/client.js";
-import cookieParser from 'cookie-parser';
-import authRouter from './routes/auth.js';
+import app from './app.js';
+import client from './db/client.js';
+
 const PORT = process.env.PORT ?? 3000;
 
 await client.connect();
 
 app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`)
-    
-    const app = express();
-app.use(express.json());
-app.use(cookieParser());
-app.use('/auth', authRouter);
-
+  console.log(`Listening on port ${PORT}`);
 });
