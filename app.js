7cdf823b07f44a5c40a1798e750ceae191462d17
Danielle <dani.camp1125@gmail.com>
2025-06-17 21:50:53 -0400

    added cors and adjustment to product routes

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ import cookieParser from 'cookie-parser';
 import usersRouter from './api/users.js';
 import productsRouter from './api/products.js';
 import ordersRouter from './api/orders.js';
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-
+app.use(cors({origin:["http://localhost:5174", "http://localhost:5173"]}))
 
 app.use('/users', usersRouter);
 app.use('/products', productsRouter);
