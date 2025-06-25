8aa7a26086030c095a41914fb8c5d449eb3c5aa8
Danielle <dani.camp1125@gmail.com>
2025-06-24 20:41:39 -0400

    Added reviews get to fetch on the frontend side --Dani

--- api/products.js
+++ api/products.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { getProducts, getProduct } from "../db/queries/products.js";
+import { getReviewsByProductId} from "../db/queries/reviews.js";
 
 //WILL NEED AUTHORIZATION ADDED TO IT
 
@@ -22,20 +23,28 @@ router.route("/:id").get(async (req, res) => {
 });
 
 router.route("/:id/reviews").get(async (req, res) => {
-  const id = req.params.id
-  if (!Number.isInteger(id) && id !=0) 
+try{
+  const id = parseInt(req.params.id, 10)
+  if (isNaN(id) || id < 0){ 
     return res.status(400).send({ error: "Invalid ID" })
+  }
   const product = await getProduct(id)
   if (!product){
     return res.status(404).send({ error: "Product not found" })
   }
     const reviews = await getReviewsByProductId(id)
   res.send(reviews)
+}catch(error){
+  console.error("Error fetching reviews:", error)
+  res.status(500).send({error: "Server error while fetching reviews"})
+}
 });
 
 router.route("/:id/reviews").post(async (req, res) => {
-  const id = req.params.id
-  if (!Number.isInteger(id)) return res.status(400).send({ error: "Invalid ID" })
+  const id = parseInt(req.params.id, 10)
+  if (isNaN(id) || id < 0){
+    return res.status(400).send({ error: "Invalid ID" })
+  } 
   const product = await getProduct(id)
   if (!product) return res.status(404).send({ error: "Product not found" })
   const { rating, comment } = req.body
